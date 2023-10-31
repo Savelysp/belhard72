ce5dd9ecd5ea194d606b97bf7a640acd2a6afe3c
Savelysp <savelysp@gmail.com>
2023-10-31 22:54:42 +0300

    master conflict

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ print("new conflict")
 print("new conflict")
 
 print("still creating conflict")
+print("new lict")
 print("new conflict")
 print("new conflict")
-print("new conflict")
-print("new conflict")
+print("new nflict")
 print("still creating conflict")
 print("still creating conflict")
 print("new conflict")
 
-print("still creating conflict")
+print("stireating conflict")
 print("still creating conflict")
 print("still creating conflict")
 
