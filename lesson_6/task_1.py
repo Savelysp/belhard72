69f1051a25ad4e69bfa0aefb845156f10c513ee2
Savelysp <savelysp@gmail.com>
2023-11-17 14:51:53 +0300

    done

--- lesson_6/task_1.py
+++ lesson_6/task_1.py
@@ -9,16 +9,17 @@ def convert(dec_number: int = None, bin_number: str = None):
         if dec_number == 1:
             return bin_result
         dec_number //= 2
-        return bin_convert(dec_number) + bin_result
+        return convert_to_bin(dec_number) + bin_result
+
     def convert_to_dec(bin_number):
         number = 0
         for i in range(1, len(bin_number) + 1):
             if bin_number[-i] == '1':
                 number += 2 ** (i - 1)
         return number
-    if dec_number == None:
-        return convert_to_bin(bin_number)
-    elif bin_number == None:
-        return convert_to_dec(dec_number)
+    if not bin_number:
+        return convert_to_bin(dec_number)
+    elif not dec_number:
+        return convert_to_dec(bin_number)
     else:
         return convert_to_bin(dec_number), convert_to_dec(bin_number)
