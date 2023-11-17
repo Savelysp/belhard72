4ffac2abc4dab98e6e3232c223846b2f581be904
Savelysp <savelysp@gmail.com>
2023-11-17 14:54:56 +0300

    done

--- lesson_6/task_3.py
+++ lesson_6/task_3.py
@@ -1,7 +1,7 @@
 # Дан список чисел и на вход поступает число N, необходимо сместить список на
 # указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
-def list_shift(requested_list: list, N: int):
-    while len(requested_list) - N:
+def list_shift(requested_list: list, n: int):
+    while len(requested_list) - n:
         requested_list.append(requested_list.pop(0))
-        N += 1
+        n += 1
     return requested_list
--- lesson_6/task_5.py
+++ lesson_6/task_5.py
@@ -1,6 +1,8 @@
 # Дан список чисел, необходимо его развернуть без использования метода revese и
 # функции reversed, а так же дополнительного списка и среза
 original_list = [1, 2, 3, 4, 5, 6, 7]
+
+
 def list_reverse(requested_list: list):
     for i in range(len(requested_list)):
         requested_list.insert(i, requested_list.pop())
