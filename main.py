3ad6c74668f0128d7aeba754d6e638cf3ceef273
Savelysp <savelysp@gmail.com>
2023-11-20 19:15:24 +0300

    done

--- main.py
+++
@@ -1,7 +0,0 @@
-user_text = input("введите текст ")
-end_of_first_word = user_text.index(" ")
-start_of_last_word = user_text.rindex(" ") + 1
-first_word = user_text[:end_of_first_word]
-last_word = user_text[start_of_last_word:]
-center_of_text = user_text[end_of_first_word:start_of_last_word]
-print(last_word + center_of_text + first_word)
